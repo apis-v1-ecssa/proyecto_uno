b85d0c3ca5361523af78b47ccffcbc0d5b245f2f
RenHec <31858903+RenHec@users.noreply.github.com>
2021-01-20 20:59:53 -0600

    Configuracion de conexión a la base de datos SQL Server 2014

--- config/database.php
+++ config/database.php
@@ -93,12 +93,12 @@ return [
 
         'sqlsrv_web' => [
             'driver' => 'sqlsrv',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => '',
+            'host' => '172.31.253.62',
+            'port' => '1433',
+            'database' => 'CONECTIVIDAD',
+            'username' => 'sa',
+            'password' => 'Ecssa2015.',
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
