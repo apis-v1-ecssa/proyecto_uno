b23d47243b31c71cf4e1219848073487876d41ed
Renato <emisor.tarea@gmail.com>
2021-03-14 10:11:25 -0600

    Detalle Pantalla

--- app/Models/V1/Seguridad/Rol.php
+++ app/Models/V1/Seguridad/Rol.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Rol extends Model
 {
     use SoftDeletes;
-    protected $dateFormat = 'Y-d-m H:i:s';
     /**
      * The connection name for the model.
      *
